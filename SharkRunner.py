928af4f21a659c13b3c95a00e26a6df47dabc1f1
Andreas Petrou-Zeniou <apetrouzeniou@csus..org>
2019-12-08 23:06:32 -0800

    Update SharkRunner.py

--- SharkRunner.py
+++ SharkRunner.py
@@ -275,7 +275,7 @@ def main():
             fish2 = Fish(GUIList[5],GUIList[6],"west",False,True,False,"DNE")
             fish3 = Fish(GUIList[10],GUIList[11],"west",False,True,False,"DNE")
             fishListObjects = [fish1,fish2,fish3] #use this list to efficiently cycle through fish objects in repetitive sequences, order is 1, 2, 3
-
+            
             #construct shark, gather coordinates to set flee status of each fish, then set direction as well
             
             shark = Shark()
