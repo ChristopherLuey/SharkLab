45296edc3476ee91851289c7ea2b6f4a8db37b6e
Chrisotpher Luey <cluey21@csus.org>
2019-12-13 16:11:48 -0800

    summary

--- Shark.py
+++ Shark.py
@@ -6,16 +6,30 @@
 from random import randrange
 
 class Shark:
+    """This class creates a "shark" which stores the theoretical positions and states,
+    while following the requirements and design specifications.
+    The shark calculates how it should change positions based on inputted theoretical fish coordinates and states.
+    It initializes at theoretical coordinates of 7, 2.
+
+    Attributes:
+        self.x: current theoretical x position of shark
+        self.y: current theoretical y position of shark
+        self.dir: theoretical direction the shark is facing
+        self.chasing: current theoretical fish the shark is chasing (0=noFishChasing, 1=chasingFish1, 2=chasingFish2, 3=chasingFish3)
+    """
+
     def __init__(self):
+        """Function creates the "shark" object which stores the position and state of the shark, while altering the position
+        in relation to inputted fish coordinates."""
         self.x, self.y, self.dir, self.chasing = 7,2, 'East', 0
 
 
     def sharkTurn(self, fishList):
-        """Function runs the shark's turn.
+        """Function alters the x and y position of the shark in order to move the shark.
+        New position is stored in shark object as self.x and self.y.
 
         Args:
-            fishList: list of fish
-        positions and states in format [f1x, f1y, f1dir, f1flee, f1alive, f2x, f2y, ...]
+            fishList: list of fish positions and states in format [f1x, f1y, f1dir, f1flee, f1alive, f2x, f2y, ...]
         """
         # Find the fish the shark is chasing
         self.calculateFishChasing(fishList)
@@ -35,18 +49,31 @@ class Shark:
 
 
     def getPosition(self):
+        """Function returns the current theoretical position of the shark.
+
+        Returns:
+            x: x position of the shark
+            y: y position of the shark
+        """
         return self.x, self.y
 
 
     def getSharkList(self):
+        """Function returns the current theoretical position and state of the shark
+        to be passed into SharkGUI.
+
+        Returns:
+            sharkList: list of shark state and attributes in format [x, y, dir, chasing]
+        """
         return [self.x, self.y, self.dir, self.chasing]
 
 
     # Shark Helper Methods - Not Included in API Since They Should Not Be Called Outside of This Class
     def calculateFishChasing(self, fishList):
-        """
+        """Function calculates which fish the shark should be chasing.
+
         Args:
-            fishList: List of fish attributes.
+            fishList: List of fish attributes in format [fish1x, fish1y, fish1dir, fish1flee, fish1IsAlive, fish2x, ...]
         """
         # Save the lowest fish distance, closest fish index, and how many fish need to be randomly chosen to pursue
         lowestFishDist, closeFishIndex, randomFishChooser = 1000, self.chasing, []
@@ -68,28 +95,27 @@ class Shark:
             elif fishDist == lowestFishDist and alive and not(self.chasing == closeFishIndex):
                 randomFishChooser.append(i+1)
 
-        # Check if the list has fish to be randomly chosen
-        if bool(randomFishChooser):
-            randomFishChooser.append(closeFishIndex)
+        randomFishChooser.append(closeFishIndex)
+        for fish in randomFishChooser:
             # Check whether the fish the shark is currently chasing is in the list
-            for fish in randomFishChooser:
-                if fish == self.chasing:
-                    closeFishIndex = fish
-                    break
-                else:
-                    # Randomly choose the fish the shark is chasing
-                    closeFishIndex = randomFishChooser[randrange(0, len(randomFishChooser))]
-
+            if fish == self.chasing:
+                closeFishIndex = fish
+                break
+            else:
+                # Randomly choose the fish the shark is chasing
+                closeFishIndex = randomFishChooser[randrange(0, len(randomFishChooser))]
         self.chasing = closeFishIndex
 
 
     def move(self, fishx, fishy):
-        """
-        Function changes the shark position to swim to the fish.
+        """Function changes the shark position to swim to the fish.
+
         Args:
             fishx: Closest fish x-position
             fishy: Closest fish y-position
         """
+        # Check for the direction that the shark should move in relation to the closest fish
+        # Flip the inequality sign and swap variables to test for all 4 directions
         dir = ""
         for i in [0,1,2,3]:
             if self.x > fishx:
